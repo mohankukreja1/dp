4ba094ba08bc7e2396296a0774ff5a77f07369e4
mohan <mohankukreja1@gmail.com>
2018-01-27 21:14:47 +0530

    minimum coins needed
    Signed-off-by: mohan <mohankukreja1@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <climits>
+#include <algorithm>
 using namespace std;
 
 int fibb(int n){
@@ -32,15 +34,52 @@ int bottomdp(int n){
     return dp[n];
 }
 
-int ladderrec(int n){
+int ladderrec(int n,int k){
     if(n==0){
         return 1;
     }
     if(n<0){
         return 0;
     }
+int ans=0;
+   for(int j=1;j<=k;j++){
+    ans += ladderrec(n-j,k);
+   }
+    return ans;
+}
 
-    return ladderrec(n-1)+ladderrec(n-2)+ladderrec(n-3);
+int laddertopdown(int n,int * dp){
+    if(n==0){
+        dp[n]=1;
+        return 1;
+    }
+    if(n<0){
+        return 0;
+    }
+    if(dp[n]!= -1){
+        return dp[n];
+    }
+        else{
+        dp[n]=dp[n-1]+dp[n-2]+dp[n-3];
+        return dp[n];
+    }
+
+}
+
+int coinsneeded(int coins[],int amount,int n){
+    if(amount==0){
+        return 0;
+    }
+    int ans=INT_MAX;
+    for(int i=0;i<n;i++){
+        if(amount-coins[i]>=0){
+            int small=coinsneeded(coins,amount-coins[i],n);
+            if(small!=INT_MAX){
+                ans=min(ans,small+1);
+            }
+        }
+    }
+    return ans;
 }
 
 
@@ -52,9 +91,12 @@ int main() {
     for(int i=0;i<100;i++){
         dp[i]=-1;
     }
+    int coins[]={1,7,10};
 //    cout<<fibb(n)<<endl;
 //    cout<<fibdp(k,dp)<<endl;
 //    cout<<bottomdp(n)<<endl;
-    cout<<ladderrec(n)<<endl;
+    cout<<ladderrec(n,3)<<endl;
+    //cout<<laddertopdown(n,dp)<<endl;
+    cout<<coinsneeded(coins,15,3)<<endl;
     return 0;
 }
